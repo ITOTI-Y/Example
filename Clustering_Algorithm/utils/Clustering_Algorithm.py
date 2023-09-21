da0f074c14dcdf8e22a1958dfc574f3ba2e5c580
pan-b <zhen_yu_@outlook.com>
2023-09-21 09:16:57 +0800

    modified:   Clustering_Algorithm/Clustering_Algorithm.ipynb 	modified:   Clustering_Algorithm/utils/Clustering_Algorithm.py

--- Clustering_Algorithm/utils/Clustering_Algorithm.py
+++ Clustering_Algorithm/utils/Clustering_Algorithm.py
@@ -3,18 +3,20 @@ import plotly.express as px
 import pandas as pd
 
 class Cluster:
-    process_centers = torch.empty(0)
-    process_node = torch.empty(0)
-
-    def __init__(self,data,k,max_iters,tol=1e-4):
+    def __init__(self,data) -> None:
         self.data = data
+        self.data_tensor = torch.tensor(self.data.values)
+        self.lenght = self.data_tensor.shape[0]
+
+    def K_means(self,k:int,max_iters:int,tol:float=1e-4,process:bool=False):
+        self.process_centers = torch.empty(0)
+        self.process_node = torch.empty(0)
         self.k = k
         self.max_iters = max_iters
         self.tol = tol
-        self.data_tensor = torch.tensor(data.values)
-        self.lenght = self.data_tensor.shape[0]
         self.init_centers()
-    
+        self.train(process=process)
+
     def init_centers(self):
         random_index = torch.randperm(self.data_tensor.shape[0])
         random_index = random_index[:self.k]
@@ -70,3 +72,26 @@ class Cluster:
                             animation_group='category',color='category',width=600,height=600,opacity=0.8
                             )
         fig.show()
+
+    def DBSCAN(self,eps=0.5,min_samples=5):
+        self.eps = eps
+        self.min_samples = min_samples
+        self.fit()
+
+    def fit(self):
+        data = self.data_tensor
+        self.cluster_labels = torch.full((self.lenght,),-1)
+        cluster_num = 0
+
+        for i in range(self.lenght):
+            if self.cluster_labels[i] != -1:
+                continue
+            distances = self.cal_distance(data[i],data)
+            neighbors = torch.where(distances < self.eps)[0]
+
+            if len(neighbors) < self.min_samples:
+                self.cluster_labels[i] = -1
+                continue
+            else:
+                self.cluster_labels[neighbors] = cluster_num
+                cluster_num += 1
